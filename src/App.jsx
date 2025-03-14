05f302667fa4dbf2b8d42a98fa405b8154633a08
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2025-03-14 12:46:19 +0530

    header components

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
+import Header from './components/header'
 
 export default function App() {
   return (
     <Router>
+      <Header />
       <Routes >
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
