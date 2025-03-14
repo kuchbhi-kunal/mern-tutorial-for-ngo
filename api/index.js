33f28777045b317f5199e21dd5b720e602f25867
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2025-03-14 23:33:04 +0530

    test api

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
 
 dotenv.config();
-mongoose.connect( process.env.MONGO ).then( () => {
-	  console.log('Connected to MongoDB!');
-} ).catch( ( err ) => {
-	  console.log('Failed to connect to MongoDB!', err);
-}
-);
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB!", err);
+  });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+  console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRouter);
