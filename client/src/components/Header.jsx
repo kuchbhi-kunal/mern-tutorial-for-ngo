876c93c1ba61568e60c25060ec0beb16379b13c1
Kunal Thakkar <78646652+kuchbhi-kunal@users.noreply.github.com>
2025-03-14 13:01:15 +0530

    create and run server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { FaSearch } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
